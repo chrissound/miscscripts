4cb50bbef6b8634dee6e3267e02a18560e7c5428
Chris Stryczynski <chris@soundshare.co.uk>
2019-04-21 09:47:23 +0000

    ranger cd

--- rangerCd.sh
+++ rangerCd.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+set -x
 
 # xdotool windowactivate $(xdotool search --name "^ranger$") \
 #  && xdotool type ":cd ${1}" \
 #  && xdotool key Return
 
-echo "cd ${1}" >> /tmp/ranger-ipc.*
-xdotool windowactivate $(xdotool search --name "^ranger$") \
-
+for f in /tmp/ranger-ipc.*; do
+  echo "cd ${1}" > "$f"
+done
+xdotool windowactivate "$(xdotool search --name "^ranger$")"
+xdotool keydown alt && xdotool type m && xdotool keyup alt
 
 # notes
 # ranger explain
